b97179e1cfa549c89e8124da57752e44af030730
soulware1 <slop@example.com>
2026-02-03 16:50:12 -0600

    wadawdads

---
+++ main.lua
@@ -0,0 +1,62 @@
+-- STANDARD ATLASES
+
+SMODS.Atlas({
+    key = "modicon", 
+    path = "ModIcon.png", 
+    px = 34,
+    py = 34,
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "balatro", 
+    path = "balatro.png", 
+    px = 333,
+    py = 216,
+    prefix_config = { key = false },
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas({
+    key = "CustomDecks", 
+    path = "CustomDecks.png", 
+    px = 71,
+    py = 95,
+    atlas_table = "ASSET_ATLAS"
+})
+
+-- ASSERTS
+
+assert(SMODS.load_file("src/jokers/example.lua"))() -- The example joker
+
+-- Unsure what this does
+local NFS = require("nativefs")
+to_big = to_big or function(a) return a end
+lenient_bignum = lenient_bignum or function(a) return a end
+
+SMODS.current_mod.optional_features = function()
+    return {
+        cardareas = {} 
+    }
+end
+
+-- Main Menu Colours (Credit to Cryptid and JoyousSpring)
+local game_main_menu_ref = Game.main_menu
+function Game:main_menu(change_context)
+    local ret = game_main_menu_ref(self, change_context)
+
+        local colours = { c1 = HEX("3F528C"), c2 = HEX("89ACEB") }
+        G.SPLASH_BACK:define_draw_steps({
+            {
+                shader = "splash",
+                send = {
+                    { name = "time",       ref_table = G.TIMERS, ref_value = "REAL_SHADER" },
+                    { name = "vort_speed", val = 0.4 },
+                    { name = "colour_1",   ref_table = colours,  ref_value = "c1" },
+                    { name = "colour_2",   ref_table = colours,      ref_value = "c2" },
+                },
+            },
+        })
+
+    return ret
+end
