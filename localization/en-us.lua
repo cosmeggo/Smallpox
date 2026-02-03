b1b99b892ec5094cfd625b7ffe6918d33c0a9ccf
soulware1 <slop@example.com>
2026-02-03 16:48:14 -0600

    wunk

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,6 +1,29 @@
 return {
     descriptions = {
+        Back = {
+            b_smallpox_showman = {
+                name = {
+                    "Showman Deck",
+                },
+                text = {
+                    "{C:attention}Joker{}, {C:tarot}Tarot{}, {C:planet}Planet{},",
+                    "and {C:spectral}Spectral{} cards may",
+                    "appear multiple times",
+                    "{s:0.8,C:inactive}Art by Inky",
+                },
+                unlock = {
+                    "{C:attention}Oh, this is the greatest show!",
+                    "{C:attention}Oh, this is the greatest show!",
+                }
+            },
+        },
         Joker = {
+            j_smallpox_examplejoker = {
+                name = 'Joker+',
+                text = {
+                    "{C:mult}+#1#{} Mult"
+                }
+            },
             j_smallpox_grinch = {
                 name = 'The Grinch',
                 text = {
--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
--- ATLASES
+-- STANDARD ATLASES
 
 SMODS.Atlas({
     key = "modicon", 
@@ -18,8 +18,8 @@ SMODS.Atlas({
 })
 
 SMODS.Atlas({
-    key = "CustomJokers", 
-    path = "CustomJokers.png", 
+    key = "CustomDecks", 
+    path = "CustomDecks.png", 
     px = 71,
     py = 95,
     atlas_table = "ASSET_ATLAS"
@@ -27,7 +27,7 @@ SMODS.Atlas({
 
 -- ASSERTS
 
-assert(SMODS.load_file("src/jokers.lua"))()
+assert(SMODS.load_file("src/jokers/example.lua"))() -- The example joker
 
 -- Unsure what this does
 local NFS = require("nativefs")
@@ -38,4 +38,25 @@ SMODS.current_mod.optional_features = function()
     return {
         cardareas = {} 
     }
+end
+
+-- Main Menu Colours (Credit to Cryptid and JoyousSpring)
+local game_main_menu_ref = Game.main_menu
+function Game:main_menu(change_context)
+    local ret = game_main_menu_ref(self, change_context)
+
+        local colours = { c1 = HEX("3F528C"), c2 = HEX("89ACEB") }
+        G.SPLASH_BACK:define_draw_steps({
+            {
+                shader = "splash",
+                send = {
+                    { name = "time",       ref_table = G.TIMERS, ref_value = "REAL_SHADER" },
+                    { name = "vort_speed", val = 0.4 },
+                    { name = "colour_1",   ref_table = colours,  ref_value = "c1" },
+                    { name = "colour_2",   ref_table = colours,      ref_value = "c2" },
+                },
+            },
+        })
+
+    return ret
 end
