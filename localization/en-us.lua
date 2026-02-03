7a9103ceb921f58ef8937893d12216cfa1d78460
Typ0Codes <137425228+Typ0Codes@users.noreply.github.com>
2026-02-02 18:43:28 -0700

    card working
    yipee

---
+++ localization/en-us.lua
@@ -0,0 +1,13 @@
+return {
+    descriptions = {
+            Joker = {
+                j_smallpox_blackjack = {
+                                name = "Blackjack",
+                                text = {
+                                    "If you {C:attention}play{} two",
+                                    "cards that total 21,",
+                                },
+                            },
+                        },
+                    },
+        }
--- src/jokers.lua
+++ src/jokers.lua
@@ -21,3 +21,119 @@ SMODS.Joker {
         end
     end
 }
+
+SMODS.Joker {
+    key = "blackjack",
+    blueprint_compat = true,
+    rarity = 3,
+    cost = 4,
+    atlas = "CustomJokers",
+    pos = { x = 3, y = 1 },
+
+    config = { extra = { min = 3, max = 20, moneymin = 1, moneymax = 20} },
+
+    loc_txt = {
+        name = "Blackjack",
+        text = { "" }
+    },
+
+    loc_vars = function(self, info_queue, card)
+
+        local r_mults = {}
+        for i = math.floor(card.ability.extra.min), math.floor(card.ability.extra.max) do
+            r_mults[#r_mults + 1] = tostring(i)
+        end
+
+        local main_end = {
+
+            {
+                n = G.UIT.T,
+                config = {
+                    text = "X",
+                    colour = G.C.MULT,
+                    scale = 0.32
+                }
+            },
+
+            {
+                n = G.UIT.O,
+                config = {
+                    object = DynaText({
+                        string = r_mults,
+                        colours = { G.C.MULT },
+                        pop_in_rate = 9999999,
+                        silent = true,
+                        random_element = true,
+                        pop_delay = 0.2011,
+                        scale = 0.32,
+                        min_cycle_time = 0
+                    })
+                }
+            },
+
+
+            {
+                n = G.UIT.T,
+                config = {
+                    text = " Mult and ",
+                    colour = G.C.UI.TEXT_DARK,
+                    scale = 0.32
+                }
+            },
+
+            {
+                n = G.UIT.O,
+                config = {
+                    object = DynaText({
+                        string = r_mults,
+                        colours = { G.C.MONEY },
+                        pop_in_rate = 9999999,
+                        silent = true,
+                        random_element = true,
+                        pop_delay = 0.2011,
+                        scale = 0.32,
+                        min_cycle_time = 0
+                    })
+                }
+            },
+
+            {
+                n = G.UIT.T,
+                config = {
+                    text = "$",
+                    colour = G.C.MONEY,
+                    scale = 0.32
+                }
+            },
+        }
+
+        return { main_end = main_end }
+    end,
+
+    calculate = function(self, card, context)
+        if context.joker_main and context.full_hand then
+            local faces = 0
+            local aces = 0
+
+            for _, c in ipairs(context.full_hand) do
+                if c:get_id() == 11 or c:get_id() == 12 or c:get_id() == 13 or c:get_id() == 10 then
+                    faces = faces + 1
+                end
+                if c:get_id() == 14 then
+                    aces = aces + 1
+                end
+            end
+
+
+            if faces == 1 and aces == 1 then
+                return {
+                    Xmult = pseudorandom('BJrandMult', card.ability.extra.min, card.ability.extra.max),
+                    dollars  = math.floor(pseudorandom('BJrandMoney', card.ability.extra.moneymin, card.ability.extra.moneymax) + 0.5),
+                    message = "BlackJack!",
+                    colour = G.C.MULT
+                }
+            end
+        end
+    end
+    
+}
