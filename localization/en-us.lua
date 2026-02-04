57d81c6ac5a6fc39dcfe60b40f7cc30cb5f1431f
wingedcatgirl <69098783+wingedcatgirl@users.noreply.github.com>
2026-02-03 20:47:06 -0500

    add vaccine

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -24,6 +24,22 @@ return {
                     "{C:mult}+#1#{} Mult"
                 }
             },
+            j_smallpox_mys_minty_vaccine = {
+                name = "Vaccine",
+                text = {
+                    "{X:dark_edition}^#1#{} Mult if all",
+                    "other Jokers are",
+                    "from other mods"
+                }
+            },
+            j_smallpox_mys_minty_vaccine_alt = {
+                name = "Vaccine",
+                text = {
+                    "{X:dark_edition}^#1#{} Mult if",
+                    "no other Jokers",
+                    "are from {V:1}Smallpox"
+                }
+            }
         },
     }
 }
---
+++ localization/en-us/mys-minty.lua
@@ -0,0 +1,24 @@
+--for reference, and/or if you ever figure out the thing
+
+return {
+    descriptions = {
+        Joker = {
+            j_smallpox_mys_minty_vaccine = {
+                name = "Vaccine",
+                text = {
+                    "{X:dark_edition}^#1#{} Mult if all",
+                    "other Jokers are",
+                    "from other mods"
+                }
+            },
+            j_smallpox_mys_minty_vaccine_alt = {
+                name = "Vaccine",
+                text = {
+                    "{X:dark_edition}^#1#{} Mult if",
+                    "no other Jokers",
+                    "are from {V:1}Smallpox"
+                }
+            }
+        }
+    }
+}
