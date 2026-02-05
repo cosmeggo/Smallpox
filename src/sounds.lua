6c04a0571370497ac3a12b98530f36337e4a58aa
LasagnaFelidae <87911854+LasagnaFelidae@users.noreply.github.com>
2026-02-05 16:45:13 +0100

    Refactor sound definitions and add new sound

--- src/sounds.lua
+++ src/sounds.lua
@@ -1,13 +1,19 @@
-SMODS.Sound{
+SMODS.Sound({
     key="miller_laugh",
     path="devil3.ogg",
     pitch=1,
     volume=0.5,
-}
+})
 
-SMODS.Sound{
+SMODS.Sound({
     key="miller_drain",
     path="drain.ogg",
     pitch=1,
     volume=0.3,
-}
+})
+
+SMODS.Sound({
+    key = "tatsu_sfx", 
+    path = "tatsu.mp3", 
+    sync = true,
+})
