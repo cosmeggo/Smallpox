87d7ed9c76767ff3e92fa9bb964a0ad405fa730e
LasagnaFelidae <87911854+LasagnaFelidae@users.noreply.github.com>
2026-02-05 16:41:08 +0100

    Update sound effects in miller.lua

--- src/jokers/miller.lua
+++ src/jokers/miller.lua
@@ -26,7 +26,7 @@ SMODS.Joker{
         if context.setting_blind and G.GAME.blind.in_blind then
                 return {
                     message = "Drained", colour = G.C.RED,
-                    play_sound("smallpox_drain"),
+                    play_sound("smallpox_miller_drain"),
                     func = function()
                         G.GAME.blind.chips = G.GAME.blind.chips * 0.75
                         G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
@@ -34,7 +34,7 @@ SMODS.Joker{
                 }
         end
         if context.end_of_round and context.game_over then
-        return {play_sound("smallpox_laugh")}
+        return {play_sound("smallpox_miller_laugh")}
         end
     end
 }
