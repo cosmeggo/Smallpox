727c0a46a3ee79af4dea74c8f0fdd5732a6c6036
pi-cubed-cat <xxcadengamingxx@gmail.com>
2026-02-06 13:17:10 +0800

    parity fixes 2

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -18,16 +18,14 @@ return {
             },
         },
         Joker = {
-            Joker = {
-                j_smallpox_smallpox = {
-                    name = 'Smallpox',
-                    text = {
-                        "{X:mult,C:white} X#1# {} Mult",
-                        "{C:green}#2# in #3#{} chance for a",
-                        "random Joker to become",
-                        "{V:1}infected{} with {C:attention}Smallpox{}"
-                    }
-                },
+            j_smallpox_smallpox = {
+                name = 'Smallpox',
+                text = {
+                    "{X:mult,C:white} X#1# {} Mult",
+                    "{C:green}#2# in #3#{} chance for a",
+                    "random Joker to become",
+                    "{V:1}infected{} with {C:attention}Smallpox{}"
+                }
             },
             j_smallpox_spikedball = {
                 name = 'Spiked Ball',
