54df0c2233764dedab1b6f4f2c2ab2e6c4d3eb4b
Rainstar760 <121758006+Rainstar760@users.noreply.github.com>
2026-02-07 20:02:03 +0200

    carote

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -27,6 +27,29 @@ return {
                     "{V:1}infected{} with {C:attention}Smallpox{}"
                 }
             }
+        },
+        Joker = {
+            j_smallpox_smallpox = {
+                name = 'Smallpox',
+                text = {
+                    "{X:mult,C:white} X#1# {} Mult",
+                    "{C:green}#2# in #3#{} chance for a",
+                    "random Joker to become",
+                    "{V:1}infected{} with {C:attention}Smallpox{}"
+                }
+            },
+            j_smallpox_roomtemperaturecarrot = {
+                name = 'Room Temperature Carrot',
+                text = {
+                    "Whenever you gain {C:money}${}, also gain",
+                    "{C:attention}+#3# Carrocoins{} per {C:money}${}",
+                    "{C:attention}Carrocoins{} can be used to either {C:attention}upgrade{}",
+                    "{X:mult,C:white}XMult{} and {X:chips,C:white}XChips{} by {X:attention,C:white}+X#4#{} per {C:attention}#5# Carrocoin{}",
+                    "or can be {C:attention}converted{} into {C:money}${} at a rate of",
+                    "{C:money}$1{} per {C:attention}#6# Carrocoins{}",
+                    "{C:inactive}(Currently {C:attention}#1# Carrocoins{C:inactive} and {X:attention,C:white}X#2#{C:inactive} {X:mult,C:white}XMult{C:inactive} and {X:chips,C:white}XChips{C:inactive})",
+                }
+            }
         }
     }
 }
