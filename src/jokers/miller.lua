6383db4a56d84df6f88c592459ca81c86d59af49
Typ0Codes <137425228+Typ0Codes@users.noreply.github.com>
2026-02-13 22:02:49 -0700

    no jumpscares mode

--- src/jokers/miller.lua
+++ src/jokers/miller.lua
@@ -28,16 +28,20 @@ SMODS.Joker{
     end,
     calculate = function(self, card, context)
         if context.setting_blind and G.GAME.blind.in_blind then
+                if SPOX_CONFIG.SPOX_Jumpscares == false then
+                    return{play_sound("smallpox_miller_drain")}
+                end
                 return {
                     message = "Drained", colour = G.C.RED,
-                    play_sound("smallpox_miller_drain"),
+                    
                     func = function()
                         G.GAME.blind.chips = G.GAME.blind.chips * (1 - card.ability.extra.percentage)
                         G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
                     end
                 }
+                
         end
-        if context.end_of_round and context.game_over then
+        if context.end_of_round and context.game_over and not SPOX_CONFIG.SPOX_Jumpscares then
         return {play_sound("smallpox_miller_laugh")}
         end
     end
