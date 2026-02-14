6a96b93e1dc5310a05b44a115fb48399b73523b5
LasagnaFelidae <87911854+LasagnaFelidae@users.noreply.github.com>
2026-02-14 07:09:55 +0100

    Create iwillnever.lua

---
+++ src/jokers/iwillnever.lua
@@ -0,0 +1,42 @@
+SMODS.Joker {
+    atlas = 'iwillnever',
+    pos = { x = 0, y = 0 },
+    pools = {["Smallpox"] = true },
+    key = "smallpox_iwillnever",
+	  pronouns = "she_they",
+    rarity = 1,
+    cost = 5,
+  	config = {
+  		extra = {
+  			xmult = 3
+  			}, 
+  		jokerforge = {
+  			sucks = false,
+  			set = false,
+  			},
+  	},
+    loc_vars = function(self, info_queue, card)
+		return { vars = {card.ability.extra.xmult } }
+    end,
+	
+  	add_to_deck = function(self,card,from_debuff)
+  		if card.ability.jokerforge.set == false then
+  			local sucks = pseudorandom("jokerforgesucks"..G.GAME.round..G.GAME.pseudorandom.seed, 0, 1)
+  			if sucks == 0 then
+  				card.ability.jokerforge.sucks = false
+  			else
+  				card.ability.jokerforge.sucks = true
+  			end
+  			card.ability.jokerforge.set = true
+  		end
+  	end,
+
+    calculate = function(self, card, context)
+		if context.joker_main and card.ability.jokerforge.sucks == true then
+			return {
+			xmult = card.ability.extra.xmult
+			}
+		end
+    end,
+    blueprint_compat = true,
+}
