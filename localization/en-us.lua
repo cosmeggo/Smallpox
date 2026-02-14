dcba3a38cada8a6e5db3adba2659e6dfdc8a0643
LasagnaFelidae <87911854+LasagnaFelidae@users.noreply.github.com>
2026-02-14 07:34:32 +0100

    Update en-us.lua

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -59,7 +59,8 @@ return {
 					"Gives {X:mult,C:white}X3{} Mult if",
 					"{C:green}JokerForge{} sucks"
                 	}
-            },
+            	},
+			},
             j_smallpox_eyejoker = {
                 name = {
                   'See Through The Joke',
@@ -109,7 +110,8 @@ return {
                     "Gain {C:money}Money{} based on the amount of owned {C:attention}Jokers{}",
                     "{C:inactive}Currently: {C:mult}+#2# Mult{C:inactive}, {X:mult,C:white}X#1#{C:inactive} XMult, and {C:money}#3#${}",
                     "{s:0.9,C:inactive}Updates when Blind is selected"
-                    },{
+                    },
+					{
                         "{s:0.9,C:inactive}It's TV Time; with your friend, Mr. Puzzles!",
                         "{s:0.9,C:inactive}It's TV Time; take a break from your troubles!",
                         "{s:0.9,C:inactive}WTF IS A COPYRIGHT 2023-2026"
@@ -964,4 +966,5 @@ return {
   },        
 
 }
+
 
