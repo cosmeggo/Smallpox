abbda48d22d44441ebd36706bfad8d6656c2ee92
Plasma <plasmaphrase@gmail.com>
2026-02-23 22:09:46 +0000

    Updated to v1.0.2~BETA

--- main.lua
+++ main.lua
@@ -50,56 +50,56 @@ assert(SMODS.load_file("./src/boosters.lua"))() -- BOOSTERS
 
 
 ---COMMON
-assert(SMODS.load_file("./src/jokers/example.lua"))() -- The example joker our chud son
+assert(SMODS.load_file("./src/jokers/jokerplus.lua"))() -- The Example Joker (our chud son)
 assert(SMODS.load_file("./src/jokers/agency.lua"))() -- Agency, The by Myst, a: Inky
 assert(SMODS.load_file("./src/jokers/ampup.lua"))() -- Amp Up by M0x3s
 assert(SMODS.load_file("./src/jokers/astro.lua"))() -- Astro by Astro, p: LasagnaFelidae
 assert(SMODS.load_file("./src/jokers/bliss.lua"))() -- Bliss by LasagnaFelidae
-assert(SMODS.load_file("./src/jokers/lexi_fire_alarm.lua"))() -- Fire Alarm by Lexi
-assert(SMODS.load_file("./src/jokers/iwillnever.lua"))() -- I will never... by Edward Robinson, p: LasagnaFelidae
-assert(SMODS.load_file("./src/jokers/ren_metaldetector.lua"))() -- Metal Detector by Ren
-assert(SMODS.load_file("./src/jokers/GhostSalt.lua"))() -- No Littering by GhostSalt
+assert(SMODS.load_file("./src/jokers/firealarm.lua"))() -- Fire Alarm by Lexi
+assert(SMODS.load_file("./src/jokers/neverjokerforge.lua"))() -- I will never... by Edward Robinson, p: LasagnaFelidae
+assert(SMODS.load_file("./src/jokers/metaldetector.lua"))() -- Metal Detector by Ren
+assert(SMODS.load_file("./src/jokers/nolittering.lua"))() -- No Littering by GhostSalt
 assert(SMODS.load_file("./src/jokers/os.JOKER.lua"))() -- os.JOKER by Nxkoo, a: Typ0
 assert(SMODS.load_file("./src/jokers/theriantropy.lua"))() -- Theriantropy by Jewel
 ---UNCOMMON
 assert(SMODS.load_file("./src/jokers/antique_chair.lua"))() -- Antique Chair by cloudzXIII
-assert(SMODS.load_file("./src/jokers/Atlas.lua"))() -- Atlas by ABuffZucchini
-assert(SMODS.load_file("./src/jokers/typ0.lua"))() -- Blackjack by Typ0
+assert(SMODS.load_file("./src/jokers/atlas.lua"))() -- Atlas by ABuffZucchini
+assert(SMODS.load_file("./src/jokers/blackjack.lua"))() -- Blackjack by Typ0
 assert(SMODS.load_file("./src/jokers/factoryline.lua"))() -- Factory Line by Lily Felli
 assert(SMODS.load_file("./src/jokers/missingposter.lua"))() -- Missing Poster by LFMoth
-assert(SMODS.load_file("./src/jokers/ProgrammerSocks.lua"))() -- Programmer Socks by Proto
-assert(SMODS.load_file("./src/jokers/inky.lua"))() -- Restaurant Menu by Inky, p: Typ0
+assert(SMODS.load_file("./src/jokers/programmersocks.lua"))() -- Programmer Socks by Proto
+assert(SMODS.load_file("./src/jokers/restaurantmenu.lua"))() -- Restaurant Menu by Inky, p: Typ0
 assert(SMODS.load_file("./src/jokers/settheory.lua"))() -- Set Theory by Arta
-assert(SMODS.load_file("./src/jokers/tjetsu_sumi.lua"))() -- Sumi by Tsutje
-assert(SMODS.load_file("./src/jokers/pi_cubed.lua"))() -- Spike Ball by Pi_cubed
-assert(SMODS.load_file("./src/jokers/meta.lua"))() -- Susie by Meta
+assert(SMODS.load_file("./src/jokers/sumi.lua"))() -- Sumi by Tsutje
+assert(SMODS.load_file("./src/jokers/spikeball.lua"))() -- Spike Ball by Pi_cubed
+assert(SMODS.load_file("./src/jokers/susie.lua"))() -- Susie by Meta
 assert(SMODS.load_file("./src/jokers/grinch.lua"))() -- Grinch by Soulware
 assert(SMODS.load_file("./src/jokers/vaccine.lua"))() -- Vaccine by mys.minty
 
 ---RARE
-assert(SMODS.load_file("./src/jokers/ruby.lua"))() -- Birthright by Ruby
-assert(SMODS.load_file("./src/jokers/tatsu.lua"))() -- Butterfly Effect by Tatsu, p: Typ0
-assert(SMODS.load_file("./src/jokers/demon_core.lua"))() -- Demon Core by ThunderEdge
-assert(SMODS.load_file("./src/jokers/breuhh.lua"))() -- Functions by Breuhh
-assert(SMODS.load_file("./src/jokers/sillyzteto.lua"))() -- Kasane Teto by Sillyz
-assert(SMODS.load_file("./src/jokers/hunter.lua"))() -- Notebook Sketch by Hunter
-assert(SMODS.load_file("./src/jokers/ouro.lua"))() -- Ouroboros by candycanearter
-assert(SMODS.load_file("./src/jokers/claus dies.lua"))() -- PK Thunder by isotypical
+assert(SMODS.load_file("./src/jokers/birthright.lua"))() -- Birthright by Ruby
+assert(SMODS.load_file("./src/jokers/butterfly.lua"))() -- Butterfly Effect by Tatsu, p: Typ0
+assert(SMODS.load_file("./src/jokers/demoncore.lua"))() -- Demon Core by ThunderEdge
+assert(SMODS.load_file("./src/jokers/functions.lua"))() -- Functions by Breuhh
+assert(SMODS.load_file("./src/jokers/kasaneteto.lua"))() -- Kasane Teto by Sillyz
+assert(SMODS.load_file("./src/jokers/notebook.lua"))() -- Notebook Sketch by Hunter
+assert(SMODS.load_file("./src/jokers/ouroboros.lua"))() -- Ouroboros by candycanearter
+assert(SMODS.load_file("./src/jokers/pkthunder.lua"))() -- PK Thunder by isotypical
 assert(SMODS.load_file("./src/jokers/polymorphine.lua"))() -- Polymorphine by NinjaBanana, a: LasagnaFelidae
 assert(SMODS.load_file("./src/jokers/puzzlevision.lua"))() -- Puzzlevision by FireIce
-assert(SMODS.load_file("./src/jokers/tigerthawk.lua"))() -- Rock Singer by TigerTHawk
+assert(SMODS.load_file("./src/jokers/rocksinger.lua"))() -- Rock Singer by TigerTHawk
 assert(SMODS.load_file("./src/jokers/roomtemperaturecarrot.lua"))() -- Room Temp. Carrot by Rainstar
 -- RARE (SMALLPOX)
-assert(SMODS.load_file("./src/jokers/notmario.lua"))() -- Fable of the Joke-Maker by notmario
+assert(SMODS.load_file("./src/jokers/fablemirror.lua"))() -- Fable of the Joke-Maker by notmario
 assert(SMODS.load_file("./src/jokers/smallpox.lua"))() -- Smallpox by Plasma
 assert(SMODS.load_file("./src/jokers/scales_of_justice.lua"))() -- Scales of Justice by Revo
 assert(SMODS.load_file("./src/jokers/seethroughthejoke.lua"))() -- See Through the Joke by Flynn
-assert(SMODS.load_file("./src/jokers/missingnumber.lua"))() -- Stained Glass Window by missingnumber
-assert(SMODS.load_file("./src/jokers/miller.lua"))() -- VOIDWALKER by Abel
+assert(SMODS.load_file("./src/jokers/stainedglass.lua"))() -- Stained Glass Window by missingnumber
+assert(SMODS.load_file("./src/jokers/voidwalker.lua"))() -- VOIDWALKER by Abel
 
 ---LEGENDARY
-assert(SMODS.load_file("./src/jokers/Egbert.lua"))() -- John Egbert, by FirstTry
-assert(SMODS.load_file("./src/jokers/tech.lua"))() -- Pox Of The Wild by Tech, p: LasagnaFelidae, a: Plasma
+assert(SMODS.load_file("./src/jokers/johnegbert.lua"))() -- John Egbert, by FirstTry
+assert(SMODS.load_file("./src/jokers/poxofthewild.lua"))() -- Pox Of The Wild by Tech, p: LasagnaFelidae, a: Plasma
 assert(SMODS.load_file("./src/jokers/coin.lua"))() -- THE COIN by Lyre, a: Lyre, p(debug): LasagnaFelidae, 
 
 --uncatagorized
